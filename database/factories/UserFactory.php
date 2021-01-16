ffb6bcfb606bd3623aeaab74ee5a2bfc0a3df3ee
Donvalent <Donvalent@yandex.ru>
2021-01-16 16:24:22 +0300

    fix error

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -35,7 +35,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'gender' => $genders[array_rand($genders)],
             'email' => $this->faker->unique()->safeEmail,
-            'phone' => $this->faker->tollFreePhoneNumber,
+            'phone' => $this->faker->phoneNumber,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
